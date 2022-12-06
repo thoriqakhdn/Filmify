aa7496f1b9ae31989f5fd394b49dca9578e7e48d
thoriq <akshazero@gmail.com>
2022-12-06 20:38:28 +0700

    0.9.2
    Menambahkan Trending

---
+++ public/js/trending.js
@@ -0,0 +1,24 @@
+var swiper = new Swiper(".slide-content", {
+    slidesPerView: 4,
+    spaceBetween: 25,
+    loop: true,
+    centerSlide: 'true',
+    fade: 'true',
+    gragCursor: 'true',
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+
+    breakpoints: {
+       0: {
+            slidesPerView: 1,
+        },
+        520: {
+            slidesPerView: 2,
+        },
+        950: {
+            slidesPerView: 3,
+        },
+    },
+  });
